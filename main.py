38540c4d768f14905b62997cd95f1eab164fc0d5
Chris Acheson <mail@chrisacheson.net>
2021-01-22 08:01:32 -0500

    Basic procedural map generation

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
 from colors import Colors
+from procgen import generate_dungeon
 
 
 def main() -> None:
@@ -13,6 +13,9 @@ def main() -> None:
     screen_height = 50
     map_width = 80
     map_height = 45
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(path="16x16-sb-ascii.png",
                                           columns=16, rows=16,
@@ -25,7 +28,11 @@ def main() -> None:
     npc = Entity(x=screen_width // 2 - 5, y=screen_height // 2, char="@",
                  color=Colors.yellow)
     entities = [npc, player]
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(max_rooms=max_rooms,
+                                room_min_size=room_min_size,
+                                room_max_size=room_max_size,
+                                map_width=map_width, map_height=map_height,
+                                player=player)
 
     engine = Engine(entities=entities, event_handler=event_handler,
                     game_map=game_map, player=player)
