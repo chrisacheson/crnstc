896b4543aff29a5e01aa2c92be7e299e415a5fb6
Chris Acheson <mail@chrisacheson.net>
2023-02-10 21:45:38 -0500

    Draw textured rectangle

--- crnstc/definitions.py
+++ crnstc/definitions.py
@@ -3,6 +3,7 @@ import pathlib
 ROOT_PATH = pathlib.Path(__file__).absolute().parent.parent
 ASSETS_PATH = ROOT_PATH / "assets"
 SHADERS_PATH = ASSETS_PATH / "shaders"
+TEXTURES_PATH = ASSETS_PATH / "textures"
 
 # FPS_CAP = None
 FPS_CAP = 60
--- crnstc/ui/__init__.py
+++ crnstc/ui/__init__.py
@@ -6,6 +6,7 @@ from glfw import GLFW
 import numpy as np
 from OpenGL import GL as gl
 from OpenGL.GL import shaders as glsl
+import PIL.Image
 
 from crnstc import definitions as defs
 
@@ -30,12 +31,54 @@ class UserInterface:
         glfw.make_context_current(self.window)
 
         gl.glClearColor(0, 0, 0, 1)
+        gl.glEnable(gl.GL_BLEND)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
 
-        # x, y, z, r, g, b
-        vertices = (0.0, 1.0, 0.0, 1.0, 0.0, 0.0,
-                    1.0, -1.0, 0.0, 0.0, 1.0, 0.0,
-                    -1.0, -1.0, 0.0, 0.0, 0.0, 1.0)
-        vertex_elements = 6
+        self.texture = gl.glGenTextures(1)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.texture)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S,
+                           gl.GL_REPEAT)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T,
+                           gl.GL_REPEAT)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER,
+                           gl.GL_NEAREST_MIPMAP_LINEAR)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER,
+                           gl.GL_NEAREST)
+        filepath = defs.TEXTURES_PATH / "16x16-square_outline.png"
+
+        with PIL.Image.open(filepath, mode="r") as image:
+            image_width, image_height = image.size
+            image = image.convert("RGBA")
+            image_data = image.tobytes()
+            gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA,
+                            image_width, image_height, 0, gl.GL_RGBA,
+                            gl.GL_UNSIGNED_BYTE, image_data)
+
+        gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
+        gl.glActiveTexture(gl.GL_TEXTURE0)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.texture)
+
+        # x, y, z, r, g, b, s, t
+        vertices = (
+            -0.5, 0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+            0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            -0.5, -0.5, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+            0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+            -0.5, -0.5, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0,
+        )
+        index = 0
+        position_index = index
+        position_elements = 3
+        index += position_elements
+        color_index = index
+        color_elements = 3
+        index += color_elements
+        texture_index = index
+        texture_elements = 2
+        index += texture_elements
+        vertex_elements = position_elements + color_elements + texture_elements
         element_bytes = 4
         stride = vertex_elements * element_bytes
         self.vertex_count = len(vertices) // vertex_elements
@@ -48,12 +91,17 @@ class UserInterface:
         gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.nbytes, vertices,
                         gl.GL_STATIC_DRAW)
         gl.glEnableVertexAttribArray(0)
-        gl.glVertexAttribPointer(0, self.vertex_count, gl.GL_FLOAT,
-                                 gl.GL_FALSE, stride, ctypes.c_void_p(0))
+        position_pointer = ctypes.c_void_p(position_index * element_bytes)
+        gl.glVertexAttribPointer(0, position_elements, gl.GL_FLOAT,
+                                 gl.GL_FALSE, stride, position_pointer)
         gl.glEnableVertexAttribArray(1)
-        color_pointer = ctypes.c_void_p(self.vertex_count * element_bytes)
-        gl.glVertexAttribPointer(1, self.vertex_count, gl.GL_FLOAT,
+        color_pointer = ctypes.c_void_p(color_index * element_bytes)
+        gl.glVertexAttribPointer(1, color_elements, gl.GL_FLOAT,
                                  gl.GL_FALSE, stride, color_pointer)
+        gl.glEnableVertexAttribArray(2)
+        texture_pointer = ctypes.c_void_p(texture_index * element_bytes)
+        gl.glVertexAttribPointer(2, texture_elements, gl.GL_FLOAT,
+                                 gl.GL_FALSE, stride, texture_pointer)
 
         with (defs.SHADERS_PATH / "vertex.glsl").open("r") as f:
             vertex_src = f.readlines()
@@ -66,10 +114,11 @@ class UserInterface:
             glsl.compileShader(fragment_src, gl.GL_FRAGMENT_SHADER),
         )
         gl.glUseProgram(self.shader)
+        gl.glUniform1i(gl.glGetUniformLocation(self.shader, "imageTexture"), 0)
 
     def render(self):
         glfw.poll_events()
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
         gl.glBindVertexArray(self.vao)
         gl.glUseProgram(self.shader)
         gl.glDrawArrays(gl.GL_TRIANGLES, 0, self.vertex_count)
@@ -79,6 +128,7 @@ class UserInterface:
     def quit(self):
         gl.glDeleteVertexArrays(1, (self.vao,))
         gl.glDeleteBuffers(1, (self.vbo,))
+        gl.glDeleteTextures(1, (self.texture,))
         glfw.terminate()
 
     @property
